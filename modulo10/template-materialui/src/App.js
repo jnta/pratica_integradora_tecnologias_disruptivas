d689ba190192dd7adb24202aa2ffcd06dbd71fd4
Jonata <jonata.a@outlook.com>
2023-11-19 20:49:23 -0300

    feat(login): add login page

--- modulo10/template-materialui/src/App.js
+++ modulo10/template-materialui/src/App.js
@@ -1,11 +1,23 @@
-import Header from "./components/header";
-import ListarTarefa from "./pages/tarefa/ListarTarefa";
+import React, { useState } from 'react';
+import Header from './components/header';
+import ListarTarefa from './pages/tarefa/ListarTarefa';
+import LoginPage from './pages/login/LoginPage';
 
 function App() {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  const handleLogin = (status) => {
+    setIsAuthenticated(status);
+  };
+
   return (
     <div className="App">
       <Header />
-      <ListarTarefa />
+      {isAuthenticated ? (
+        <ListarTarefa />
+      ) : (
+        <LoginPage onLogin={handleLogin} />
+      )}
     </div>
   );
 }
---
+++ modulo10/template-materialui/src/pages/login/LoginPage.jsx
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+import './LoginPage.css'
+
+const LoginPage = ({ onLogin }) => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = () => {
+    onLogin(username, password);
+  };
+
+  return (
+    <div className="login-container">
+      <h2>Login</h2>
+      <form>
+        <label htmlFor="username">Username:</label>
+        <input
+          type="text"
+          id="username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+
+        <label htmlFor="password">Password:</label>
+        <input
+          type="password"
+          id="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+
+        <button type="button" onClick={handleLogin}>
+          Login
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default LoginPage;
